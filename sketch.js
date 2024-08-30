7401fafd0327a6ab7af390601ed8b4b5941e0a55
KEA KAMIYA <KSK23A@FSU.EDU>
2024-08-30 16:15:29 -0400

    added up to green

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
 function setup() {
+  //creates a canvas 600 by 400 
   createCanvas(600, 400);
 }
 
 function draw() {
-  background(125);
+  //changing the numbers in the parathesis change the color of the background
+  background(135, 206, 235);
+  //circle sun 
+  fill("yellow");
+  stroke ("orange");
+  strokeWeight(20); //outline
+circle (550, 50, 100); 
+stroke(0);
+strokeweight(1);
+fill("green");
+rect(0, 200, 600, 200);
 }
