2223b99a29d2bb05b3a8ae4e688b89009ea5b262
KEA KAMIYA <KSK23A@FSU.EDU>
2024-08-30 16:23:50 -0400

    Working Tutorial

--- sketch.js
+++ sketch.js
@@ -12,7 +12,10 @@ function draw() {
   strokeWeight(20); //outline
 circle (550, 50, 100); 
 stroke(0);
-strokeweight(1);
+strokeWeight(1);
 fill("green");
 rect(0, 200, 600, 200);
+textSize(75)
+  text ("🌸", 100, 250)
+  text("🐞", mouseX, mouseY)
 }
